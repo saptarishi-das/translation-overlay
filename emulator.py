eb2fc8cd8696d1e918cc410e2e3f365243aabec6
Saptarishi  Das <saptarishi.das28@gmail.com>
2021-06-03 11:59:07 +0000

    test results

--- emulator.py
+++ emulator.py
@@ -6,5 +6,8 @@ ovl = TranslationOverlay()
 _, _, f_name = next(walk('input'))
 for name in f_name:
     print('processing: {}'.format(name))
-    image_names = ovl.create_image_with_translation_overlay('input/{}'.format(name))
-    print(image_names)
+    try:
+        image_names = ovl.create_image_with_translation_overlay('input/{}'.format(name))
+        print(image_names)
+    except Exception as e :
+        print(e)
